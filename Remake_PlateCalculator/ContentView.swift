55a58ce054e8b3432460ff6bc32ac35821233bac
grantrudow <58617204+wrangler13@users.noreply.github.com>
2020-03-04 07:46:20 -0500

    initial project setup

---
+++ Remake_PlateCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Remake_PlateCalculator
+//
+//  Created by Grant Rudow on 3/4/20.
+//  Copyright © 2020 Grant Rudow. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
